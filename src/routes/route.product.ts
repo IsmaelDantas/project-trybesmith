7b75564b2eaa200381f5c3a80c24bd96d81a8f3f
IsmaelDantas <ismcondan2019@gmail.com>
2022-12-04 15:35:15 -0300

    add req 06

--- src/routes/route.product.ts
+++ src/routes/route.product.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import ControllerProduct from '../controller/controller.product';
+import { validateAmount, validateName } from '../middlewares/middleware.product.validation';
 
 const controllerProduct = new ControllerProduct();
 const routerProduct = express.Router();
 
-routerProduct.post('/', (req, res) => controllerProduct.create(req, res));
+routerProduct.post(
+  '/',
+  validateName,
+  validateAmount,
+  (req, res) => controllerProduct.create(req, res),
+);
 routerProduct.get('/', (req, res) => controllerProduct.findAll(req, res));
 
 export default routerProduct;
